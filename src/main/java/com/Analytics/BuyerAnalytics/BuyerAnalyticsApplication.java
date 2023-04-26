616f1182aee7797ffe067e591be1b7e787df1e57
Chitransh Saxena <chitransh033@gmail.com>
2023-04-26 07:16:14 +0530

    Add main file

---
+++ src/main/java/com/Analytics/BuyerAnalytics/BuyerAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.Analytics.BuyerAnalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyerAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyerAnalyticsApplication.class, args);
+	}
+
+}
